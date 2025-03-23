4e3087953f56f48c4ac9924e4470d84eb51c9ba5
luci <lucille_blain@live.com>
2025-03-23 00:54:35 -0400

    added disuccsion post db

---
+++ travel_guardian/src/model/discussions.ts
@@ -0,0 +1,11 @@
+"use server";
+import { getPrisma } from "@/lib/db";
+import { discussions } from "@prisma/client";
+
+export async function getDiscussionHistory() {}
+
+export async function createDiscussion(){}
+
+export async function updateDiscussion(){}
+
+export async function deleteDiscussion(){}
---
+++ travel_guardian/src/model/reply.ts
@@ -0,0 +1,41 @@
+"use server";
+import { getPrisma } from "@/lib/db";
+import {reply} from "@prisma/client";
+
+// Fetch reply history for a discussion or user
+export async function getReplyHistory(creatorId: number): Promise<reply[]> {
+    const client = await getPrisma();
+    return client.reply.findMany({
+        where: { creatorId: creatorId },
+    });
+}
+
+// Create a new reply
+export async function createReply(creatorId: number, discussionId: number, content: string, parentId?: number){
+    const client = await getPrisma();
+    return client.reply.create({
+        data: {
+            content,
+            creatorId,
+            discussionId,
+            parentId,
+        },
+    });
+}
+
+// Update a reply
+export async function updateReply(replyId: number, content: string){
+    const client = await getPrisma();
+    return client.reply.update({
+        where: { id: replyId },
+        data: { content },
+    });
+}
+
+// Delete a reply
+export async function deleteReply(replyId: number){
+    const client = await getPrisma();
+    return  client.reply.delete({
+        where: { id: replyId },
+    });
+}
