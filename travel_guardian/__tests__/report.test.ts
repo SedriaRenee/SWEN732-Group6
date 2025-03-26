12736db251eeb28ba0fcf4f0a2e14ac9cf7d5ec8
Owen <ojr4316@rit.edu>
2025-03-26 01:34:45 -0400

    add loc page

--- travel_guardian/__tests__/report.test.ts
+++ travel_guardian/__tests__/report.test.ts
@@ -6,6 +6,5 @@ test("Get Reports", async () => {
     const roc = (await searchLocation("Rochester"))[0];
     const reports = await getReports(roc.id);
     assert.isArray(reports, "Reports is not an array");
-    assert(reports.length == 0, "Reports is not empty");
-});
-
+    assert(reports.length > 0, "Reports is empty");
+}); 
