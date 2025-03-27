9eb528ed89b568dc98cfaaf9f532ce43ad7e2e8d
Owen <ojr4316@rit.edu>
2025-03-27 11:36:39 -0400

    mock report

--- travel_guardian/__tests__/report.test.ts
+++ travel_guardian/__tests__/report.test.ts
@@ -1,10 +1,11 @@
 import {test, assert} from 'vitest';
 import { getReports } from '@/model/report';
 import { searchLocation } from '@/model/location';
+import { prismaMock } from './__mocks__/prismaMocks';
 
 test("Get Reports", async () => {
     const roc = (await searchLocation("Rochester"))[0];
-    const reports = await getReports(roc.id);
+    const reports = await prismaMock.report.findMany({where: {locId: roc.id}});
     assert.isArray(reports, "Reports is not an array");
     assert(reports.length == 0, "Reports is not empty");
 }); 
